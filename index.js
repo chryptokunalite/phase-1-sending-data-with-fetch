2ebd85d0c4f624fa6152e3d41377f10ec347a337
Allan Saenz <allan.d.saenz@gmail.com>
2023-07-26 20:30:59 -0400

    done

--- index.js
+++ index.js
@@ -1,1 +1,59 @@
 // Add your code here
+// const configurationObject = {
+//     method: "POST",
+//     headers: {
+//         "Content-Type": "application/json",
+//         "Accept": "application/json",
+//       },
+//       body: JSON.stringify({
+//         dogName: "Byron",
+//         dogBreed: "Poodle",
+//       }),
+// };
+// fetch("http://localhost:3000/dogs", configurationObject)
+// .then(function (response) {
+//     return response.json();
+//   })
+//   .then(function (object) {
+//     console.log(object);
+//   })
+//   .catch(function (error) {
+//     alert("Bad things! Ragnarők!");
+//     console.log(error.message);
+//   });
+
+
+  function submitData(userName, userEmail){
+    return fetch("http://localhost:3000/users",{
+      method: 'POST',
+      headers: { 
+        "content-type": "application/json",
+        "Accept": "application/json",
+      },
+      body:JSON.stringify({
+        name : userName,
+        email : userEmail,
+      })
+    })
+    .then(function(response){
+      return response.json()
+    })
+    .then(function(object){
+      const newId = object.id
+      const idElement = document.createElement('p')
+      idElement.textContent = newId
+      document.body.append(idElement)
+    })
+    .catch(function (error) {
+      const errorMessage = "Unauthorized Access"
+      const errorElement = document.createElement('p')
+      errorElement.textContent = errorMessage
+      document.body.append(errorMessage)
+
+    })
+  }
+  
+      
+
+
+ 
